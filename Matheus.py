08f58b4c2b434b9e57bf296c60a979eff1a6ec05
Lara <126460907+larapdalese@users.noreply.github.com>
2024-08-12 16:06:16 -0300

    terceiro commit

--- Matheus.py
+++ Matheus.py
@@ -1,3 +1,11 @@
 def cumprimento(texto):
     return (f"Olá, {texto}!")
 print(cumprimento("Lara Peclart Dalese"))
+
+import random
+def sorteio():
+     numeros = [random.randint(0, 10) for _ in range(3)]
+     print(numeros)
+     media = sum(numeros) / len(numeros)
+     return media
+print(sorteio())
